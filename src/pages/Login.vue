d947ee642c2e42f665e62c5d43a2524709090a04
Yupi Li <592789970@qq.com>
2019-01-13 17:18:07 +0800

    add element-ui

---
+++ src/pages/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="login">
+        <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
