a66ec1c2090205acedf41b77ea1906b9b29b22a2
Yupi Li <592789970@qq.com>
2019-01-13 18:45:15 +0800

    add login page

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
+    /* common css */
+    .center {
+        margin: 0 auto;
+    }
+
+    .width50 {
+        width: 50%;
     }
 </style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,65 @@
 <template>
     <div id="login">
-        <img alt="Vue logo" src="../assets/logo.png">
+        <el-form ref="form" :model="loginForm" id="loginForm">
+            <img alt="logo" src="../assets/logo.png" class="logo">
+            <el-form-item>
+                <el-input v-model="loginForm.account" placeholder="请输入账号" autofocus></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input v-model="loginForm.passwd" placeholder="请输入密码" @keyup.enter.native="onSubmit"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" class="width50" @click="onSubmit" round>登录
+                </el-button>
+            </el-form-item>
+        </el-form>
+        <VueCanvasNest :config="{color:'0,0,0', count: 150}"/>
     </div>
 </template>
 
 <script>
+    import VueCanvasNest from 'vue-canvas-nest'
+
     export default {
-        name: "Login"
+        name: "Login",
+        components: {
+            VueCanvasNest
+        },
+        data() {
+            return {
+                loginForm: {
+                    account: '',
+                    passwd: ''
+                }
+            }
+        },
+        methods: {
+            onSubmit() {
+                this.$message({
+                    message: '登录成功',
+                    type: 'success'
+                });
+            }
+        }
     }
 </script>
 
 <style scoped>
+
+    #loginForm {
+        width: 260px;
+        height: 360px;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-top: -200px;
+        margin-left: -130px;
+    }
+
+    .logo {
+        padding: 20px;
+    }
+
 
 </style>
