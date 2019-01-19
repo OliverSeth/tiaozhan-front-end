630cae8a45ba03821c6613ad4a4f89c4f4bc8a59
SwanEiffel <1415025584@qq.com>
2019-01-19 10:32:49 +0800

    update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,114 @@
 <template>
-    <div id="home">
-        home
-    </div>
+    <el-container id="home">
+        <div class="sidebar-wrapper">
+            <el-aside class="sidebar" width="217px">
+                <el-menu default-active="dashboard" class="menubar" router>
+                    <el-menu-item index="dashboard">
+                        <i class="el-icon-menu"></i>
+                        <span slot="title">仪表盘</span>
+                    </el-menu-item>
+                    <el-menu-item index="cloth-examine">
+                        <i class="el-icon-search"></i>
+                        <span slot="title">布料检测</span>
+                    </el-menu-item>
+                    <el-menu-item index="examine-report">
+                        <i class="el-icon-document"></i>
+                        <span slot="title">检测报告</span>
+                    </el-menu-item>
+                    <el-menu-item index="user-manage">
+                        <i class="el-icon-setting"></i>
+                        <span slot="title">用户管理</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+        </div>
+        <el-container style="margin-left: 200px">
+            <el-header style="height: auto;padding: 5px 20px;">
+                <div class="left title">{{$route.meta.title}}</div>
+                <el-menu :default-active="activeIndex" mode="horizontal" class="right">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <img src="../assets/logo.png" class="headpic" alt=""/>
+                            超级管理员 李鱼皮
+                        </template>
+                        <el-menu-item index="1-1">个人</el-menu-item>
+                        <el-menu-item index="1-2" @click="logout">注销</el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-header>
+            <el-main>
+                <router-view/>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
     export default {
-        name: 'home'
-    }
+        name: "Home",
+        data() {
+            return {
+                activeIndex: '1',
+                title: '仪表盘'
+            }
+        },
+        methods: {
+            logout() {
+                this.$message({
+                    message: '已注销',
+                    type: 'success'
+                });
+                this.$route.push('/login');
+            }
+        }
+    };
 </script>
+
+<style scoped>
+    #home {
+        width: 100%;
+        height: 100%;
+    }
+
+    #home .el-menu {
+        border-bottom: none;
+    }
+
+    .sidebar-wrapper {
+        height: 100%;
+        overflow: hidden;
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        width: 200px;
+        z-index: 1000;
+        background-color: white;
+        border-right: solid 1px #e6e6e6;
+    }
+
+    .sidebar {
+        height: 100%;
+        overflow-x: hidden;
+        overflow-y: scroll;
+    }
+
+    .menubar {
+        height: 100%;
+        border: none;
+    }
+
+    #home .title {
+        margin-top: 20px;
+    }
+
+    .headpic {
+        width: 42px;
+        height: 42px;
+        border-radius: 50%;
+        border: 1px solid #ddd;
+        padding: 3px;
+        margin-right: 10px;
+    }
+</style>
+
--- src/router.js
+++ src/router.js
@@ -1,20 +1,64 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/Home.vue'
-import Login from "./pages/Login";
+import Login from "./pages/Login"
+import UserManage from './pages/UserManage'
+import ClothExamine from './pages/ClothExamine'
+import ExamineReport from './pages/ExamineReport'
+import Dashboard from "./pages/Dashboard";
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
         {
             path: '/',
-            component: Home
+            component: Home,
+            meta: {
+                title: '控制台'
+            },
+            children: [
+                {
+                    path: '/dashboard',
+                    component: Dashboard,
+                    meta: {
+                        title: '仪表盘'
+                    }
+                },
+                {
+                    path: '/user-manage',
+                    component: UserManage,
+                    meta: {
+                        title: '用户管理'
+                    }
+                }, {
+                    path: '/cloth-examine',
+                    component: ClothExamine,
+                    meta: {
+                        title: '布料检测'
+                    }
+                }, {
+                    path: '/examine-report',
+                    component: ExamineReport,
+                    meta: {
+                        title: '检测报告'
+                    }
+                }
+            ]
         }, {
             path: '/login',
             component: Login
         }
     ]
-})
+});
+
+// 切换页面title
+router.beforeEach((to, from, next) => {
+   if (to.meta.title) {
+       document.title = to.meta.title;
+   }
+   next();
+});
+export default router;
