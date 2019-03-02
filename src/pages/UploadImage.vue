c486b8642bea8e4bb4934953280a238ae93cf0b2
OliverSeth <szl980731@126.com>
2019-03-02 11:33:23 +0800

    update

--- src/pages/UploadImage.vue
+++ src/pages/UploadImage.vue
@@ -1,9 +1,9 @@
 <template>
-    <div style="width: 80%;height: 88%;position: absolute">
+    <div id="ulDiv">
         <!--<div style="margin-top: -75px">-->
         <!--<el-button type="primary" @click="uploadPhoto">上传</el-button>-->
         <!--</div>-->
-        <el-form :model="form">
+        <el-form :model="form" id="ulForm">
             <el-form-item>
                 <el-upload
                         action='#'
@@ -21,25 +21,27 @@
                 <el-button size="small" @click="returnPg">返回</el-button>
             </el-form-item>
         </el-form>
-        <el-form :model="form2">
-            <el-form-item>
-                <el-upload
-                        class="upload-demo"
-                        action="http://106.12.123.92:8081/api/v1/pictures/upload/xml/do-admin"
-                        :on-preview="handlePreview"
-                        :on-remove="handleRemove"
-                        :before-upload="handleBefore"
-                        multiple
-                        :on-exceed="handleExceed"
-                        :file-list="fileList">
-                    <i class="el-icon-plus"></i>
-                </el-upload>
-            </el-form-item>
-            <el-form-item>
-                <el-button size="small" type="primary" @click="uploadXML">上传XML文件</el-button>
-                <el-button size="small" @click="returnPg">返回</el-button>
-            </el-form-item>
-        </el-form>
+        <el-dialog :visible.sync="formVisible" title="上传xml文件" width="30%">
+            <el-form :model="form2">
+                <el-form-item>
+                    <el-upload
+                            class="upload-demo"
+                            action="http://106.12.123.92:8081/api/v1/pictures/upload/xml/do-admin"
+                            :on-preview="handlePreview"
+                            :on-remove="handleRemove"
+                            :before-upload="handleBefore"
+                            :on-exceed="handleExceed"
+                            :file-list="fileList">
+                        <i class="el-icon-plus"></i>
+                    </el-upload>
+                </el-form-item>
+                <el-form-item>
+                    <el-button size="small" type="primary" @click="uploadXML">上传XML文件</el-button>
+                    <el-button size="small" @click="returnPg">返回</el-button>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
+
         <div style="margin-top: 30px">
             <el-dialog :visible.sync="dialogVisible">
                 <img width="100%" :src="dialogImageUrl" alt="">
@@ -48,40 +50,41 @@
     </div>
 </template>
 <script>
-    import  cookies from "vue-cookies";
+    import cookies from "vue-cookies";
 
     export default {
         name: "uploadImage",
-        data(){
-            return{
-                activeIndex:'1',
+        data() {
+            return {
+                formVisible: false,
+                activeIndex: '1',
                 dialogImageUrl: '',
                 dialogVisible: false,
-                form:{},
-                form2:{},
-                file:'',
-                file2:'',
-                fileList:[],
-                fileList2:[],
+                id:0,
+                form: {},
+                form2: {},
+                file: '',
+                file2: '',
+                fileList: [],
+                fileList2: [],
             }
         },
-        methods:{
-            handleBefore(file){
+        methods: {
+            handleBefore(file) {
                 // console.log(file);
                 let fileValue = file.name;
                 // console.log(fileValue);
                 let index = fileValue.lastIndexOf('.');
-                const isXML=fileValue.substring(index)==='.xml';
+                const isXML = fileValue.substring(index) === '.xml';
                 // console.log(index);
-                if(isXML){
-                    this.file2=file;
-                }
-                else{
-                    this.$message.error('上传文件只能是 xml 格式!');;
+                if (isXML) {
+                    this.file2 = file;
+                } else {
+                    this.$message.error('上传文件只能是 xml 格式!');
                 }
                 return isXML;
             },
-            returnPg(){
+            returnPg() {
                 this.$router.push('/photo-gallery');
             },
             handlePreview(file) {
@@ -93,7 +96,6 @@
             // beforeRemove(file, fileList) {
             //     return this.$confirm(`确定移除 ${ file.name }？`);
             // },
-
             handleRemove(file, fileList) {
                 console.log(file, fileList);
             },
@@ -101,7 +103,7 @@
                 this.dialogImageUrl = file.url;
                 this.dialogVisible = true;
             },
-            beforeAvatarUpload(file,fileList) {
+            beforeAvatarUpload(file, fileList) {
                 const isJPG = file.type === 'image/jpeg';
                 const isLt5M = file.size / 1024 / 1024 < 5;
                 // this.file=file;
@@ -122,18 +124,23 @@
                 // }
                 return isJPG && isLt5M;
             },
-            uploadXML(){
+            uploadXML() {
                 let fd=new FormData();
-                let api=this.$api.userApi.savePicture;
-                fd.append("xmlFile", this.file2);
+                // console.log(this.row.picId);
+                let api={
+                    url:'http://106.12.123.92:8081/api/v1/pictures/'+this.id+'/do-admin',
+                    method:'put'
+                };
+                fd.append("xmlFile", this.file);
                 let xhr=new XMLHttpRequest();
                 let that=this;
                 xhr.open("post",'http://106.12.123.92:8081/api/v1/pictures/upload/xml/do-admin',true);
+                xhr.setRequestHeader('token',cookies.get('token'));
                 xhr.send(fd);
                 xhr.onload = function () {
                     if (xhr.readyState === xhr.DONE) {
                         if (xhr.status === 200) {
-                            console.log(xhr.responseText);
+                            // console.log(xhr.responseText);
                             let xml;
                             let arr=xhr.responseText.split('"');
                             let flag=false;
@@ -142,29 +149,43 @@
                             }
                             if(flag){
                                 for(let i=0;i<arr.length;i++){
-                                    if(arr[i]==="data"&&arr[i+1]===":"){
+                                    if(arr[i]==="url"&&arr[i+1]===":"){
                                         xml=arr[i+2];
                                         break;
                                     }
                                 }
                             }
+                            // console.log(xml);
                             api.data={
                                 xml:xml,
-                                name:'',
-                                defectType: -1,
-                                defectPosition: -1,
                             };
                             that.axios(api).then(function (response) {
-                                console.log(response);
+                                if(response.data.code===0){
+                                    that.$notify({
+                                        title: '成功',
+                                        message: '上传xml文件成功',
+                                        type: 'success'
+                                    });
+                                }
+                                else{
+                                    that.$notify.error({
+                                        title: '失败',
+                                        message: '上传失败'
+                                    });
+                                }
+                                that.formVisible=false;
+                                that.$refs.upload.clearFiles();
+                                // console.log(response);
                             })
                         }
                     }
                 }
             },
-            uploadPhoto(){
-                let fd=new FormData();
-                for(let i=0;i<this.fileList2.length;i++){
-                    fd.append("pictures",this.fileList2[i]);
+            uploadPhoto() {
+                let fd = new FormData();
+                let that = this;
+                for (let i = 0; i < this.fileList2.length; i++) {
+                    fd.append("pictures", this.fileList2[i]);
                 }
                 // fd.append("pictures", this.fileList2);
                 console.log(this.fileList2);
@@ -181,27 +202,48 @@
 
                 // let that=this;
                 // let api=that.$api.userApi.savePicture;
-                let xhr=new XMLHttpRequest();
+                let xhr = new XMLHttpRequest();
                 // let fileName=this.file.name;
                 // let index=fileName.lastIndexOf('.');
                 // console.log(index);
                 // fileName=fileName.slice(0,index);
                 // console.log(fileName);
                 // sessionStorage.setItem("name",fileName);
-                xhr.open("post",'http://106.12.123.92:8081/api/v1/pictures/upload/do-admin',true);
-                xhr.setRequestHeader('token',cookies.get('token'));
+                xhr.open("post", 'http://106.12.123.92:8081/api/v1/pictures/upload/do-admin', true);
+                xhr.setRequestHeader('token', cookies.get('token'));
                 console.log(fd.get("pictures"));
                 xhr.send(fd);
                 xhr.onload = function () {
                     if (xhr.readyState === xhr.DONE) {
                         if (xhr.status === 200) {
+                            console.log(xhr.responseText);
 
                             // let url;
-                            // let arr=xhr.responseText.split('"');
-                            // let flag=false;
-                            // for(let i=0;i<arr.length;i++){
-                            //     if(arr[i]==="成功") flag=true;
-                            // }
+                            let arr = xhr.responseText.split('"');
+                            let flag = false;
+                            for (let i = 0; i < arr.length; i++) {
+                                if (arr[i] === "成功") {
+                                    flag = true;
+                                }
+                                if(arr[i]==='data'){
+                                    // console.log(arr[i+1].substring(2,arr[i+1].length-2));
+                                    that.id=parseInt(arr[i+1].substring(2,arr[i+1].length-2));
+                                    // console.log(that.id);
+                                }
+                            }
+                            if (flag) {
+                                that.formVisible=true;
+                                that.$notify({
+                                    title: '成功',
+                                    message: '上传成功',
+                                    type: 'success'
+                                });
+                            } else {
+                                that.$notify.error({
+                                    title: '失败',
+                                    message: '上传失败'
+                                });
+                            }
                             // if(flag){
                             //     for(let i=0;i<arr.length;i++){
                             //         if(arr[i]==="data"&&arr[i+1]===":"){
@@ -266,7 +308,7 @@
                 // })
                 // this.$refs.upload.submit();
             },
-            savePicture(){
+            savePicture() {
                 // this.uploadPhoto();
 
             }
@@ -275,9 +317,21 @@
 </script>
 
 <style scoped>
-    .el-menu-demo{
+    .el-menu-demo {
         position: absolute;
         margin-top: -80.5px;
-        margin-left:50px;
+        margin-left: 50px;
+    }
+
+    #ulDiv {
+        width: 80%;
+        height: 88%;
+        position: absolute
+    }
+    #ulForm{
+        top:40%;
+        left:50%;
+        position: absolute;
+        transform: translate(-50%,-50%);
     }
 </style>
