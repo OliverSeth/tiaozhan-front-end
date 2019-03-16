45b6e1f890eff24c589cde92d414ae637e35bd39
OliverSeth <szl980731@126.com>
2019-03-16 14:34:48 +0800

    实时显示PLC状态

--- src/pages/DownloadPy.vue
+++ src/pages/DownloadPy.vue
@@ -57,7 +57,7 @@
                         url=this.options[i].pyUrl;
                     }
                 }
-                window.location.href='http://bigdata1:50070'+url;
+                window.location.href='http://106.12.123.92:5555'+url;
                 // this.axios({
                 //     // url:'http://bigdata1:50070/webhdfs/v1/upload/model-py/19-02/23/bb83d19c-2696-4884-baa0-2de7dc99d1ea-1.py?op=OPEN',
                 // }).then(()=>{
