06a75a28f05e6463453adc7ca76982a4bbea311e
OliverSeth <szl980731@126.com>
2019-04-10 16:19:06 +0800

    登录界面

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,93 +1,97 @@
 <template>
-    <div id="login">
+    <div id="login" :style="bg" style="height: 100%;width: 100%;position: absolute">
         <el-form ref="form" :model="loginForm" id="loginForm">
-            <img alt="logo" src="../assets/logo2.jpg" class="logo">
-            <el-row :gutter="300"><span style="color: black;font-size: 30px;font-family:sans-serif">东华大学面料疵点检测综合管理平台</span></el-row><br>
+            <img alt="logo" src="../assets/dhu.png" width="114" height="114" class="logo">
+            <el-row :gutter="300"><span
+                    style="color: black;font-size: 30px;font-family:sans-serif">东华大学面料疵点检测综合管理平台</span></el-row>
+            <br>
 
             <el-form-item>
                 <el-input v-model="loginForm.account" placeholder="请输入账号" autofocus></el-input>
             </el-form-item>
             <el-form-item>
-                <el-input type="password" v-model="loginForm.passwd" placeholder="请输入密码" @keyup.enter.native="onSubmit"></el-input>
+                <el-input type="password" v-model="loginForm.passwd" placeholder="请输入密码"
+                          @keyup.enter.native="onSubmit"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" class="width50" style="position: relative;left: -5%" @click="register" round>注册</el-button>
-                <el-button type="primary" class="width50" style="position: relative;right: -5%" @click="onSubmit" round>登录</el-button>
+                <el-button type="primary" class="width50" style="position: relative;left: -5%" @click="register" round>
+                    注册
+                </el-button>
+                <el-button type="primary" class="width50" style="position: relative;right: -5%" @click="onSubmit" round>
+                    登录
+                </el-button>
             </el-form-item>
         </el-form>
-        <VueCanvasNest :config="{color:'0,0,0', count: 100}"/>
-        <div id="word" style="width: 100%;height: 70%;position: absolute;margin-top:600px" >
-            <el-row>
-                <el-col :span="4" push="19">
-                    <br><el-row :gutter="13"><span style="font-size: 18px" >团队名称：DHU-Cloud</span></el-row><br>
-
-                </el-col>
-
-            </el-row>
-            <br>
-
+        <!--<VueCanvasNest :config="{color:'0,0,0', count: 100}"/>-->
+        <!--<div id="word" style="width: 100%;height: 70%;position: absolute;margin-top:600px" >-->
+        <div style="top: 70%;position: absolute;left: 80%">
+            <span style="font-size: 18px;color: #b86312">团队名称：DHU-Cloud</span>
         </div>
+        <!--</div>-->
     </div>
 </template>
 
 
 <script>
-    import VueCanvasNest from 'vue-canvas-nest'
+    // import VueCanvasNest from 'vue-canvas-nest'
 
     export default {
         name: "Login",
-        components: {
-            VueCanvasNest
-        },
+        // components: {
+        //     VueCanvasNest
+        // },
         data() {
             return {
                 loginForm: {
                     account: '',
                     passwd: ''
                 },
-                path:'http://106.12.123.92:8081/api/v1/users/login/'
+                path: 'http://106.12.123.92:8081/api/v1/users/login/',
+                bg:{
+                    backgroundImage:'url('+require('../assets/bg.jpg')+')',
+                    backgroundRepeat:'no-repeat'
+                }
             }
         },
         methods: {
-            register(){
-                let api=this.$api.userApi.register;
-                let account=this.loginForm.account;
-                api.data={
-                    account:this.loginForm.account,
-                    pwd:this.loginForm.passwd,
-                    role:0
+            register() {
+                let api = this.$api.userApi.register;
+                let account = this.loginForm.account;
+                api.data = {
+                    account: this.loginForm.account,
+                    pwd: this.loginForm.passwd,
+                    role: 0
                 };
-                this.axios(api).then(response=>{
-                    let data=response.data;
-                    if(data.code===0){
+                this.axios(api).then(response => {
+                    let data = response.data;
+                    if (data.code === 0) {
                         this.$message({
                             message: '注册成功',
                             type: 'success'
                         });
                         // location.reload();
-                        this.loginForm.passwd='';
-                    }
-                    else{
+                        this.loginForm.passwd = '';
+                    } else {
                         this.$message({
                             message: '用户已经存在',
                             type: 'error'
                         });
-                        this.loginForm.account='';
-                        this.loginForm.passwd=''
+                        this.loginForm.account = '';
+                        this.loginForm.passwd = ''
                     }
                 })
             },
             onSubmit() {
-                let api=this.$api.userApi.login;
-                api.data={
-                    account:this.loginForm.account,
-                    pwd:this.loginForm.passwd
+                let api = this.$api.userApi.login;
+                api.data = {
+                    account: this.loginForm.account,
+                    pwd: this.loginForm.passwd
                 };
-                this.axios(api).then(response=>{
+                this.axios(api).then(response => {
                     console.log(response);
                     let data = response.data;
-                    if(data.code===0){
-                        let token=data.data.token;
+                    if (data.code === 0) {
+                        let token = data.data.token;
                         console.log(token);
                         this.$cookies.set("token", token, 7200);
                         console.log(token);
@@ -95,12 +99,11 @@
                             message: '登录成功',
                             type: 'success'
                         });
-                        sessionStorage.setItem("userName",this.loginForm.account);
+                        sessionStorage.setItem("userName", this.loginForm.account);
                         this.$router.push('/photo-gallery');
-                    }
-                    else{
+                    } else {
                         this.$notify.error({
-                            message:'用户名或密码错误'
+                            message: '用户名或密码错误'
                         })
                     }
                 })
@@ -127,5 +130,10 @@
         padding: 20px;
     }
 
+    /*#login{*/
+        /*height: 88%;*/
+        /*weight:80%;*/
+        /*position: absolute;*/
+    /*}*/
 
 </style>
